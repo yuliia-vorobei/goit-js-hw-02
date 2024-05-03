a88e04cd2f53782d0e8ddcb518acf3671fe4b17c
Yuliia Vorobei <vorobeju1@gmail.com>
2024-05-03 12:44:57 +0200

    js homework 2

---
+++ js/task-3.js
@@ -0,0 +1,20 @@
+function checkForSpam(message) {
+  let spamMessage = "spam";
+  let saleMessage = "sale";
+  message = message.toLowerCase();
+  //   console.log(saleMessage);
+  //   console.log(spamMessage);
+  //   console.log(message);
+  if (message.includes(saleMessage) || message.includes(spamMessage)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
